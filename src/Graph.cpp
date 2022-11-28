fc255bd62c78a286fe883ca026eb08dc91c1996d
Christina Castillo <ccastillo3@ufl.edu>
2022-11-27 20:48:52 -0500

    Add filtering and similarity scoring functionality

--- src/Graph.cpp
+++ src/Graph.cpp
@@ -1,6 +1,13 @@
 #include "Graph.hpp"
 
 // ----------  FILTERING ---------- //
+
+/**
+ * @brief Determines if a Video passes the filter.
+ * 
+ * @param currVid Video* being checked
+ * @return boolean that is true iff the Video passes the filter.
+ */
 bool Graph::passesFilter(Video* currVid) const {
     /// TODO: Make this call to the actual filter function
     /// To filter out videos with characteristics the user doesn't want to see
@@ -13,6 +20,7 @@ bool Graph::passesFilter(Video* currVid) const {
 }
 
 // ----------  MEMORY AND DATA MANAGEMENT HELPERS ---------- //
+
 void Graph::copyVals(const Graph& original) {
     for (int i = 0; i < original.rootVideoIDs.size(); i++) {
         this->rootVideoIDs.push_back(original.rootVideoIDs.at(i));
@@ -32,6 +40,17 @@ void Graph::deleteVals() {
 }
 
 // ----------  GRAPH TRAVERSAL HELPERS ---------- //
+
+/**
+ * @brief Determines if a video associated with an ID should be visited in a traversal.
+ * Considers if the ID has already been visited.
+ * Considers if there is no associated mapping from ID to Video.
+ * Considers if the associated Video passes the filter (using passesFilter).
+ * 
+ * @param visited reference to a set representing the visited IDs.
+ * @param videoID string vector of the video's ID.
+ * @return boolean that is true iff the video with that ID should be visited.
+ */
 bool Graph::addToTraversal(std::unordered_set<std::string>& visited, std::string videoID) const {
     if (visited.find(videoID) == visited.end()) {
         visited.insert(videoID);
@@ -61,6 +80,7 @@ std::vector<Video*>& result, std::unordered_set<std::string>& visited, int n) co
 }
 
 // ----------  MEMORY AND DATA MANAGEMENT FUNCTIONS ---------- //
+
 /// FIXME: Should this do anything?
 Graph::Graph() {}
 
@@ -79,6 +99,15 @@ Graph::~Graph() {
 }  
 
 // ----------  GRAPH CREATION ---------- //
+
+/**
+ * @brief Inserts a Video object to the graph.
+ * 
+ * @param stats string vector of the details of the video, in order of appearance in a file.
+ * @param relatedIDs string vector of the IDs of related videos to the video.
+ * @param isRoot boolean that is true iff a Video is a 'root' from '0.txt'.
+ * @return void.
+ */
 void Graph::insertVideo(std::vector<std::string> stats,
 std::vector<std::string> relatedIDs, bool isRoot) {
     Video* newVid = new Video(stats, relatedIDs);
@@ -90,6 +119,16 @@ std::vector<std::string> relatedIDs, bool isRoot) {
 }
 
 // ----------  GRAPH TRAVERSALS ---------- //
+
+/**
+ * @brief Gets the first n Videos passing the filter in a breadth-first traversal.
+ * Measures how long this takes.
+ * Returns fewer than n results if not enough Videos pass the filter.
+ * 
+ * @param startID string ID of a root video.
+ * @param n int number of videos to return.
+ * @return std::pair<std::vector<Video*>, double> pair of a vector of Video* and a time (in seconds).
+ */
 std::pair<std::vector<Video*>, double> Graph::traverseBreadthFirstN(std::string startID, int n) const {
     auto startTime = std::chrono::steady_clock::now();
 
@@ -131,6 +170,15 @@ std::pair<std::vector<Video*>, double> Graph::traverseBreadthFirstN(std::string 
     return std::pair<std::vector<Video*>, double>(result, runTime);
 }
 
+/**
+ * @brief Gets the first n Videos passing the filter in a postorder depth-first traversal.
+ * Measures how long this takes.
+ * Returns fewer than n results if not enough Videos pass the filter.
+ * 
+ * @param startID string ID of a root video.
+ * @param n int number of videos to return.
+ * @return std::pair<std::vector<Video*>, double> pair of a vector of Video* and a time (in seconds).
+ */
 std::pair<std::vector<Video*>, double> Graph::traversePostorderDepthFirstN(std::string startID, int n) const {
     auto startTime = std::chrono::steady_clock::now();
     std::vector<Video*> result;
@@ -156,14 +204,31 @@ std::pair<std::vector<Video*>, double> Graph::traversePostorderDepthFirstN(std::
 }
 
 // ----------  GETTERS ---------- //
+
+/**
+ * @brief Gets the number of Videos in the graph.
+ * 
+ * @return int number of Videos.
+ */
 int Graph::getSize() const {
     return size;
 }
 
+/**
+ * @brief Gets the number of 'root' Videos from '0.txt' in the graph.
+ * 
+ * @return int number of root Videos.
+ */
 int Graph::getNumRootVideos() const {
     return rootVideoIDs.size();
 }
 
+/**
+ * @brief Gets the first n 'root' Videos passing the filter.
+ * 
+ * @param n int number of videos to return.
+ * @return std::vector<Video*> of filtered videos.
+ */
 std::vector<Video*> Graph::getNFilteredRootVideos(int n) const {
     std::vector<Video*> result;
     int ind = 0;
@@ -176,6 +241,50 @@ std::vector<Video*> Graph::getNFilteredRootVideos(int n) const {
     return result;
 }
 
+
+/**
+ * @brief Gets the similarity scores between two videos.
+ * 
+ * @param firstVidID string ID of one Video.
+ * @param secondVidID string ID of the other Video.
+ * @return float of the similarity score.
+ */
+float Graph::getSimilarityScore(std::string firstVidID, std::string secondVidID) const {
+    auto firstVidIter = idToVideo.find(firstVidID);
+    auto secondVidIter = idToVideo.find(secondVidID);
+    if (firstVidIter != idToVideo.end() && secondVidIter != idToVideo.end()) {
+        return getSimilarityScore(firstVidIter->second, secondVidIter->second);
+    }
+    else {
+        return -1;
+    }
+}
+
+/**
+ * @brief Gets the similarity scores between two videos.
+ * 
+ * @param firstVid pointer to one Video.
+ * @param secondVid pointer to the other Video.
+ * @return float of the similarity score.
+ */
+float Graph::getSimilarityScore(Video* firstVid, Video* secondVid) const {
+    /// TODO: Make this call to the actual scoring function
+    return 1;
+}
+
 std::vector<std::string> Graph::getRootVideoIDs() const {
     return rootVideoIDs;
+}
+
+// ----------  SETTERS ---------- //
+
+/**
+ * @brief Sets the Settings object that defines the Video filter
+ * within the graph object.
+ * 
+ * @param filterInfo Settings object that contains filter data.
+ * @return void.
+ */
+void Graph::setFilterInfo(Settings filterInfo) {
+    this->filterInfo = filterInfo;
 }
